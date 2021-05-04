4a9f76950cac41ff120fe66fc48b4e230e412e93
Xiaoyu Feng <xiaoyufeng@192.168.1.10>
2021-05-04 17:40:41 +0800

    Update Interview questions

---
+++ 程序员面试金典_LeetCode/Interview_01_09.py
@@ -0,0 +1,6 @@
+# 面试题 01.09. 字符串轮转
+class Solution:
+    def isFlipedString(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        return s1 in (s2 + s2)
