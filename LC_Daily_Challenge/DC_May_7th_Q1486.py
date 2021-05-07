431bf0638d5586dc7db958da62e4e209147b85f5
Justin-xiaoyu-feng <79350770+Justin-xiaoyu-feng@users.noreply.github.com>
2021-05-07 19:34:30 +0800

    Add files via upload

---
+++ LC_Daily_Challenge/DC_May_7th_Q1486.py
@@ -0,0 +1,7 @@
+# 1486. 数组异或操作
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        res = 0
+        for i in range(n):
+            res ^= (start + 2*i)
+        return res
